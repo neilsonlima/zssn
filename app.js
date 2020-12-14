ec1c98341b34b6cc0ed6621eaa6a4aff5b792678
Neilson Lima <neilsonlima@gmail.com>
2020-12-13 22:34:29 -0300

    configuração com banco de dados postgresql

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 
+const db = require("./config/database");
+
+db.authenticate()
+  .then(() => console.log('Database connected...'))
+  .catch( err => console.log('Unable to connect to the database:', err));
+
+
+
 const app = express();
 
 app.get("/", (req, res) => res.send("INDEX"));
