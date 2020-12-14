b20bdd383cb67e2023a31143b2343b0afc619eaf
Neilson Lima <neilsonlima@gmail.com>
2020-12-13 22:04:01 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const app = express();
+
+app.get("/", (req, res) => res.send("INDEX"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
